979df23a2c143a62db25490264da5e2b360e1ee0
vrkeel <vladkeel@gmail.com>
2019-01-04 18:33:29 +0200

    fix known tags

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from ModelA import ModelA
 from ModelB import ModelB
 import data_parser as prs
 if __name__ == '__main__':
-    with open('res_fileB','w') as f:
-        #print("First Model:", file=f)
-        #modelA = ModelA('modelA', True)
-        #modelA.test('train.wtag', 'test.wtag',f)
-        #modelA.comp('comp.words')
-
+    with open('res_fileA','w') as f:
+        print("First Model:", file=f)
+        modelA = ModelA('modelA', True)
+        modelA.test('train.wtag', 'test.wtag',f)
+        modelA.comp('comp.words')
+    with open('res_fileB', 'w') as f:
         print("Second Model:", file=f)
         modelB = ModelB('modelB')
         modelB.test('train2.wtag', None,f)
